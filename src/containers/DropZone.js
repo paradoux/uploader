e7a997961c2752e5f3048e4b745a952ed2204bce
Axel Martin <martin.axe@live.fr>
2018-07-28 19:38:22 +0200

    Done lvl2 + lvl3 / Interrogation on db total binary update lagg

--- src/containers/DropZone.js
+++ src/containers/DropZone.js
@@ -1,37 +1,84 @@
 import React from 'react'
+import axios from 'axios'
 
 class DropZone extends React.Component {
 
     constructor() {
         super()
         this.state = {
+            id: '',
+            total: '',
             name: ''
         }
         this.displayName = this.displayName.bind(this)
+    }
+
+    getRequest = () => {
+        return axios.get('https://fhirtest.uhn.ca/baseDstu3/Binary')
+    }
+
+    componentDidMount = () => {
+        this.getRequest()
+            .then((res) => {
+                let { total } = res.data
+                this.setState({ ...this.state, total })
+            })
     }
 
     dragOverHandler = (e) => {
         e.preventDefault()
     }
 
-    handleDrop = (e) => {
-        var { name } = e.dataTransfer.items[0].getAsFile()
-        this.setState({ ...this.state, name })
-        console.log(this.state)
+    constructFile = (e) => {
+        let { name } = e.dataTransfer.files[0]
+        const data = new FormData()
+        data.append('file', e.dataTransfer.files[0])
+        data.append('name', name)
+        return data
     }
 
+    handleDrop = (e) => {
+        var data = this.constructFile(e)
+        var { name } = e.dataTransfer.files[0]
+        axios.post('https://fhirtest.uhn.ca/baseDstu3/Binary', data)
+            .then((res) => {
+                let id = res.data.issue[0].diagnostics.match(/\d/g).join("")
+                console.log(name)
+                this.setState({ ...this.state, name, id })
+                this.getTotal()
+            })
+            .catch((err) => {
+                console.error(err)
+                alert("Sorry, the document couldn't be uploaded")
+            })
+    }
+
+    getTotal = () => {
+        this.getRequest()
+            .then((res) => {
+                var { total } = res.data
+                if (total === this.state.total) {
+                    total++
+                    this.setState({ ...this.state, total })
+                }
+                else {
+                    this.setState({ ...this.state, total })
+                }
+            })
+    }
 
     displayName = () => {
-        var { name } = this.state
-        if (name !== '') {
+        var { name, id, total } = this.state
+        if (id !== '' && total !== '') {
             return (
-                <h1> Your file {name} has been uploaded !</h1>
+                <div>
+                    <h1> {`Your file ${name} has been uploaded ! Its id is : ${id}`}</h1>
+                    <h2>{`There are currently ${total} binary documents in the DataBase`}</h2>
+                </div>
             )
         }
         else { return (<h1> Upload the document you want ! </h1>) }
-
     }
-
 
     render() {
         return (

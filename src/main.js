bdcebb22dc6ab2205efc2d35d346cb18706db61a
Axel Martin <martin.axe@live.fr>
2018-07-31 10:52:16 +0200

    Correction path chokidar

--- src/main.js
+++ src/main.js
@@ -1,21 +1,16 @@
 const electron = require('electron');
-// Module to control application life.
+var fileWatcher = require("chokidar");
 const app = electron.app;
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
+
 
 const path = require('path');
 const url = require('url');
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
 function createWindow() {
-    // Create the browser window.
     mainWindow = new BrowserWindow({ width: 800, height: 600 });
 
-    // and load the index.html of the app.
     /*     const startUrl = process.env.ELECTRON_START_URL || url.format({
             pathname: path.join(__dirname, '/../build/index.html'),
             protocol: 'file:',
@@ -23,39 +18,54 @@ function createWindow() {
         }); */
     mainWindow.loadURL('http://localhost:3000');
 
-    // Open the DevTools.
     mainWindow.webContents.openDevTools();
 
-    // Emitted when the window is closed.
     mainWindow.on('closed', function () {
-        // Dereference the window object, usually you would store windows
-        // in an array if your app supports multi windows, this is the time
-        // when you should delete the corresponding element.
         mainWindow = null
     })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit()
     }
 });
 
 app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
         createWindow()
     }
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+var { dialog } = electron;
+
+function StartWatcher(path) {
+    var chokidar = require("chokidar");
+
+    var watcher = chokidar.watch(path, {
+        ignored: /[\/\\]\./,
+        persistent: true
+    });
+
+    function onWatcherReady() {
+        console.info('From here can you check for real changes, the initial scan has been completed.');
+    }
+    // Declare the listeners of the watcher
+    watcher
+        .on('add', function (path) {
+            console.log('File', path, 'has been added');
+        })
+        .on('error', function (error) {
+            console.log('Error happened', error);
+        })
+        .on('ready', onWatcherReady)
+        .on('raw', function (event, path, details) {
+            // This event should be triggered everytime something happens.
+            console.log('Raw event info:', event, path, details);
+        });
+}
+
+StartWatcher("C:/Users/Marti/Desktop/FHIR")
+
